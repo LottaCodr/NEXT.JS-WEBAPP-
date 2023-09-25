282926da8d18f58bdcf6d78fd42b984ece897d1b
King-Lota <lottanna47@gmail.com>
2023-09-25 15:19:46 +0100

    THE WEBAPP

---
+++ app/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import Logo from "./lotta.jpg";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Image src={Logo} alt="Picture of Lotta" width={70} />
+
+      <h1>Lotta HelpDesk</h1>
+      <Link href="/"> Dashboard</Link>
+      <Link href="/Tickets"> Tickets</Link>
+    </nav>
+  );
+}
